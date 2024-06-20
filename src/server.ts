e4c9425656d9d94e518f85ac79f5a8a47e151dd8
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-19 22:53:37 -0300

    Handle env with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-  .where('amoutn', 500)
-  .select('*')
 
-  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
