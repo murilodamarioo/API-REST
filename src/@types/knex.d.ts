a37afb4cd03f99c87021916dbd25042a50949cfc
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-20 00:02:31 -0300

    Fix types table transactions

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,11 +1,13 @@
-import { Knex } from 'kenx'
+import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    id: string
-    title: string
-    amount: number
-    created_at: string
-    session_id?: string
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
   }
 }
