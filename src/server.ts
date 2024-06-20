41016dad9ad183de52fdf42cfd82b352d5cbef30
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-19 21:59:21 -0300

    Environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+  .where('amoutn', 500)
+  .select('*')
 
-  return tables
+  return transaction
 })
 
 app
