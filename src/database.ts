6853d9887701d34abda776e6cdb6cdb6988417f4
Murilo Damario <murilo.gcdamario@gmail.com>
2024-06-23 19:27:42 -0300

    Production config

--- src/database.ts
+++ src/database.ts
@@ -4,10 +4,10 @@ import { env } from './env'
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not defined')
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_CLIENT === 'sqlite' ? {
     filename: env.DATABASE_URL
-  },
+  } : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
